dd62fd678be4accf980b7024ac77e643fb2cbefe
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-05-22 21:36:07 -0400

    Change yielding to passing

--- lib/minitest/spec/expect_syntax_for_block.rb
+++ lib/minitest/spec/expect_syntax_for_block.rb
@@ -1,18 +1,18 @@
 require 'minitest/spec/expect_syntax'
 
 class MiniTest::Spec::ExpectSyntaxForBlock < MiniTest::Spec::ExpectSyntax
-  BLOCK_YIELDING_EXPECTATION_REGEXES = [/silent/, /output/, /raise/, /throw/]
+  BLOCK_PASSING_EXPECTATION_REGEXES = [/silent/, /output/, /raise/, /throw/]
 
   private
 
   def expectation_names
     MiniTest::Expectations.instance_methods.select do |method|
-      detect_block_yielding_expectation method
+      detect_block_passing_expectation method
     end
   end
 
-  def detect_block_yielding_expectation method
-    BLOCK_YIELDING_EXPECTATION_REGEXES.detect do |regex|
+  def detect_block_passing_expectation method
+    BLOCK_PASSING_EXPECTATION_REGEXES.detect do |regex|
       method.to_s.match regex
     end
   end
