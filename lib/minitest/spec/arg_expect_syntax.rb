027ca78403e179fa2a405bfbf6112dd5e81b1dcf
Dave Jachimiak <dave.jachimiak@gmail.com>
2013-05-22 21:23:48 -0400

    Delete old files

--- lib/minitest/spec/arg_expect_syntax.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/spec/expect_syntax'
-
-class MiniTest::Spec::ExpectSyntaxForArg < MiniTest::Spec::ExpectSyntax
-  private
-
-  def expectation_names
-    MiniTest::Expectations.instance_methods
-  end
-end
